83cd1261fc7906de44c5a65f73cebf5833c1c557
dazinator <darrell.tunnell@googlemail.com>
2015-04-22 21:24:59 +0100

    class rename

---
+++ src/FluentGit/FluentEnumerable.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FluentGit
+{  
+
+    public class FluentEnumerable<T> : IFluentEnumerable<T>
+    {
+        private IEnumerator<T> _enumerator;
+
+        public FluentEnumerable(IEnumerator<T> enumerator)
+        {
+            _enumerator = enumerator;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return _enumerator;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return _enumerator;
+        }
+    }
+}
